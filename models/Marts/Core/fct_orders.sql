ab187e26eea01d09444cc860db9ab10abe139860
Sayaji Phulwade <sayaji.phulwade@inteliment.com>
2023-09-08 04:39:52 +0000

    ..

--- models/Marts/Core/fct_orders.sql
+++ models/Marts/Core/fct_orders.sql
@@ -12,11 +12,22 @@ payments as
 orders_payments as
 (
 
-    select  orders.order_id,
-            orders.customer_id,
-            payments.AMOUNT
-        from orders  inner join  payments 
-        on orders.order_id = payments .id 
+    select  
+            order_id,
+            sum (case when status = success then amount end ) as amount,
+        from  payments 
+        group by 1
+),
+
+final as 
+(
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(orders_payments.amount,0) as amount
+        from orders 
+        left join orders_payments using (order_id)
 )
 
-select * from orders_payments
+select * from final
